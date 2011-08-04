1a0a3db7af1031f0dc804d9d173aa31006d3e85f
moviles <moviles@mini.local>
2011-08-04 21:09:41 +0200

    Created project

---
+++ Places.xcodeproj/PlacesUITableViewController.h
@@ -0,0 +1,16 @@
+//
+//  PlacesUITableViewController.h
+//  Places
+//
+//  Created by moviles on 04/08/11.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+
+@interface PlacesUITableViewController : UITableViewController {
+    
+}
+
+@end
---
+++ Places/FlickrAPIKey.h
@@ -0,0 +1,10 @@
+//
+//  FlickrAPIKey.h
+//
+//  Created for Stanford CS193p Fall 2010
+//  Copyright Stanford University
+//
+
+// Put your Flickr API key here or nothing will work.
+
+#define FlickrAPIKey @"056d852d780b5a5fd8e940319c4e3e15"
